3cbf1276a704223b432c34b376039c52064c1ee1
Himanshu <himanshu561hi@gmail.com>
2026-03-30 09:45:29 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -27,9 +27,8 @@ app.use((req, res, next) => {
 });
 
 // Middlewares
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors("*"));
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
 
 // Routes
 app.use('/api/auth', authRoutes);
